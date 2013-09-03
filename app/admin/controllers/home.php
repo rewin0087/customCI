f80c6ae77288a7c9e249f042ca88e9de321c1c87
rewin0087 <erwin_rulezzz87@yahoo.com>
2013-09-03 09:53:08 +0800

    add home controller

---
+++ app/admin/controllers/home.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends Base_Controllers {
+	/* Constants
+	-------------------------------*/
+	/* Public Properties
+	-------------------------------*/
+	/* Protected Properties
+	-------------------------------*/
+	/* Private Properties
+	-------------------------------*/
+	protected $_meta        = array();
+    protected $_body        = array();
+    protected $_title       = '';
+    protected $_class       = '';
+    protected $_template    = '';
+	
+	 /* Magic
+    -------------------------------*/
+	/* Get
+	-------------------------------*/
+	/* Magic
+	-------------------------------*/
+	/* Public Methods
+	-------------------------------*/
+
+	public function index() {
+			
+	}
+	
+	/* Protected Methods
+	-------------------------------*/
+	
+}
